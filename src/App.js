a783b870a14cbb0ff9176fee08c833fb1c73a934
Scott Cole <sscole929@gmail.com>
2023-08-28 14:54:11 -0500

    firstFiveChapters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import { getAllTickets } from "./services/ticketService"
+import "./App.css"
 
-function App() {
+export const App = () => {
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+  const [filteredTickets, setFilteredTickets] = useState([])
+
+  useEffect(() => {
+    getAllTickets().then(ticketsArray => {
+      setAllTickets(ticketsArray)
+      console.log("tickets set")
+    })
+
+  }, [])
+
+  useEffect(() => {
+    if (showEmergencyOnly === true) {
+      const emergencyTickets = allTickets.filter(
+        (ticket) => ticket.emergency === true
+      )
+      setFilteredTickets(emergencyTickets)
+  } else {
+      setFilteredTickets(allTickets)
+    }
+  }, [showEmergencyOnly, allTickets])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tickets-container">
+    <h2>Tickets</h2>
+    <div>
+      <button className="filter-btn btn-primary" 
+        onClick={() => {
+          setShowEmergencyOnly(true)
+        }}
+      >
+          Emergency
+      </button>
+      <button className="filter-btn btn-info"
+        onClick={() => {
+          setShowEmergencyOnly(false)
+        }}
+      >
+        Show All
+      </button>
     </div>
-  );
+    <article className="tickets">
+      {filteredTickets.map(ticket => {
+        return (
+          <section className="ticket" key={ticket.id}>
+            <header className="ticket-info">#{ticket.id}</header>
+              <div>{ticket.description}</div>
+              <footer>
+              <div>
+                <div className="ticket-info">emergency</div>
+                <div>{ticket.emergency ? "yes" : "no"}</div>
+              </div>
+            </footer>
+          </section>
+        )
+      })}
+    </article>
+  </div>
+  )
 }
-
-export default App;
